4ae0b2638169f31ea0b3fee332b2a7b3878fe21a
Arthur Almeida <arthuraraujo07@hotmail.com>
2024-07-26 23:40:27 -0300

    commit add news - about

--- app.py
+++ app.py
@@ -9,14 +9,42 @@ from utils.crypto_chart_generator import (
     create_crypto_charts, create_crypto_pie_chart,
     create_crypto_volume_chart, create_crypto_bar_chart
 )
+from utils.news_fetcher import get_financial_news
 import plotly.express as px
 import pandas as pd
 import logging
+from datetime import datetime, timedelta
+import urllib.parse
 
 # Configurar logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+NEWS_API_KEY = '6068bb4a398b4076b54c1a53d024affd'  # API key fornecida
+
+# Cache para notícias
+news_cache = {
+    'ações financeiras': {'data': None, 'timestamp': None},
+    'criptomoedas': {'data': None, 'timestamp': None}
+}
+CACHE_EXPIRY = timedelta(minutes=30)  # Defina o tempo de expiração do cache
+
+def get_cached_news(query):
+    current_time = datetime.now()
+    cache_key = urllib.parse.quote(query)
+    cache = news_cache.get(cache_key, {'data': None, 'timestamp': None})
+    
+    if cache['data'] is None or current_time - cache['timestamp'] > CACHE_EXPIRY:
+        logging.debug(f"Fetching news for query: {query}")
+        news_articles = get_financial_news(NEWS_API_KEY, query)
+        if news_articles:
+            news_cache[cache_key] = {'data': news_articles[:5], 'timestamp': current_time}
+        else:
+            news_cache[cache_key] = {'data': [], 'timestamp': current_time}
+    else:
+        logging.debug(f"Using cached news for query: {query}")
+    
+    return news_cache[cache_key]['data']
 
 @app.route('/')
 def index():
@@ -29,7 +57,8 @@ def index():
             fig = create_stock_chart(df, symbol)
             charts.append(fig.to_html(full_html=False))
         symbols = get_all_symbols()
-        return render_template('index.html', charts=charts, top_stocks=top_stocks, symbols=symbols, company_names=company_names)
+        news_articles = get_cached_news('ações financeiras')
+        return render_template('index.html', charts=charts, top_stocks=top_stocks, symbols=symbols, company_names=company_names, news_articles=news_articles)
     except Exception as e:
         logging.error(f"Erro ao carregar a página inicial: {e}")
         return render_template('error.html', message=str(e))
@@ -83,13 +112,21 @@ def update_bar_chart():
         df_bar = pd.DataFrame({'Ação': names, 'Variação Percentual': percent_changes})
         fig_bar = px.bar(df_bar, x='Ação', y='Variação Percentual', title='Variação Percentual das Top 10 Ações')
         fig_bar.update_traces(textposition='outside')
-        fig_bar.update_layout(plot_bgcolor='rgba(0, 0, 0, 0.8)', paper_bgcolor='rgba(0, 0, 0, 0.8)', font_color='white', title_font_color='white', xaxis_title_font_color='white', yaxis_title_font_color='white', xaxis=dict(showgrid=False, zeroline=False), yaxis=dict(showgrid=False, zeroline=False))
+        fig_bar.update_layout(
+            plot_bgcolor='rgba(0, 0, 0, 0.8)', 
+            paper_bgcolor='rgba(0, 0, 0, 0.8)', 
+            font_color='white', 
+            title_font_color='white', 
+            xaxis_title_font_color='white', 
+            yaxis_title_font_color='white', 
+            xaxis=dict(showgrid=False, zeroline=False), 
+            yaxis=dict(showgrid=False, zeroline=False)
+        )
         graph_html_bar = fig_bar.to_html(full_html=False)
         return jsonify({'graph_html_bar': graph_html_bar})
     except Exception as e:
         logging.error(f"Erro ao atualizar o gráfico de barras: {e}")
         return jsonify({'error': str(e)}), 500
-
 
 @app.route('/update_crypto_chart', methods=['POST'])
 def update_crypto_chart():
@@ -143,8 +180,14 @@ def cryptos():
     try:
         top_cryptos = get_top_10_cryptos()
         graph_html_pie = create_crypto_pie_chart(pd.DataFrame(top_cryptos), 'total_volume', 'Distribuição de Volume das Top 10 Criptomoedas')
-        graph_html_bar = create_crypto_bar_chart(pd.DataFrame(top_cryptos), 'name', 'current_price', 'Preço Atual das Top 10 Criptomoedas')
-        return render_template('cryptos.html', top_cryptos=top_cryptos, graph_html_pie=graph_html_pie, graph_html_bar=graph_html_bar)
+        df_bar = pd.DataFrame({
+            'name': [crypto['name'] for crypto in top_cryptos],
+            'current_price': [crypto['current_price'] for crypto in top_cryptos]
+        })
+        fig_bar = create_crypto_bar_chart(df_bar, 'name', 'current_price', 'Preço Atual das Top 10 Criptomoedas')
+        graph_html_bar = fig_bar.to_html(full_html=False)
+        news_articles = get_cached_news('criptomoedas')
+        return render_template('cryptos.html', top_cryptos=top_cryptos, graph_html_pie=graph_html_pie, graph_html_bar=graph_html_bar, news_articles=news_articles)
     except Exception as e:
         logging.error(f"Erro ao carregar a página de criptomoedas: {e}")
         return render_template('error.html', message=str(e))
